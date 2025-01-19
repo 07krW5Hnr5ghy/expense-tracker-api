31ba9ddbf4272ace8c25f74cfd86b5a7257befd1
07krW5Hnr5ghy <jryn145@gmail.com>
2025-01-18 19:56:54 -0500

    refactor(main): add scripts and fix auth controller file name

--- server.js
+++ server.js
@@ -20,6 +20,15 @@ connectDB();
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/expenses', require('./routes/expenseRoutes'));
 
+// Error handling middleware (optional)
+app.use((err, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode).json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+    });
+});
+
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
